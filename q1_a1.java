6b8ff03adf520ca6bd0f3fb7a0d72c278f9ee749
NandiniChaturvedi2004 <122256725+NandiniChaturvedi2004@users.noreply.github.com>
2023-01-19 21:45:09 +0530

    Submitting Q1 of Assignment1

---
+++ q1_a1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+    public class q1_a1 {
+  
+    
+      public static void main(String[] args) {
+        // Write your code here
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+      }
+    }
